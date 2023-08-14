e035fd68058fcc6fde0bc7d3945b904b035b061e
Manuel Rivero <manuel.river.r.v@gmail.com>
2023-08-14 17:30:55 -0300

    get profile invalid token handler

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -4,35 +4,38 @@ import { validateBody } from "./../../helpers/validate/index.js";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
-
-
 export const updateSocial = {
   do: async (req, res) => {
-    console.log("update social")
+    console.log("update social");
     const { uid } = req;
-    const {instagram, facebook, twitter} = req.body;
-    const targetUser = User.findById(uid)
-    if (!targetUser){
-        res.sta
+    const { instagram, facebook, twitter } = req.body;
+    const targetUser = User.findById(uid);
+    if (!targetUser) {
+      res.sta;
     }
-    console.log("target user", targetUser)
+    console.log("target user", targetUser);
   },
 };
 
 export const getProfile = {
   do: async (req, res) => {
-    const {token} = req.cookies
-    const {slug} = req.params
+    const { token } = req.cookies;
+    const { slug } = req.params;
     let isSameUser = false;
-    const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+    try {
+      const { uid } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
 
-    const targetProfile = await User.findOne({slug:slug})
-    console.log("targetProfile", targetProfile)
-    if(targetProfile._id.toString() === uid){
-      console.log("if case")
-      isSameUser = true
+      const targetProfile = await User.findOne({ slug: slug });
+      if (targetProfile._id.toString() === uid) {
+        isSameUser = true;
+      }
+
+      return res.json({ data: { isSameUser, profileData: targetProfile } });
+    } catch (error) {
+      return res.status(401).json({
+        ok: false,
+        message: "Token no válido",
+      });
     }
-
-    return res.json({data:{isSameUser, profileData: targetProfile}})
   },
 };
