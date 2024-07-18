bd833be476d3c1804e5f1b550cea54d390fe0122
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-07-18 20:31:19 -0300

    [add] get all categories search param

--- src/controllers/category/index.js
+++ src/controllers/category/index.js
@@ -5,17 +5,17 @@ import joi from "joi";
 export const createCategory = {
   check: (req, res, next) => {
     const schema = joi.object({
-        name: joi.string().required(),
-      });
-      validateBody(req, next, schema);
+      name: joi.string().required(),
+    });
+    validateBody(req, next, schema);
   },
   do: async (req, res, next) => {
     const { name } = req.body;
     try {
       const targetCategory = await Category.findOne({
-        name:  {'$regex': `^${name}$`,$options:'i'},
+        name: { $regex: `^${name}$`, $options: "i" },
       });
-      console.log("targetCategory",targetCategory)
+      console.log("targetCategory", targetCategory);
       if (targetCategory) {
         res.status(400).json({
           ok: false,
@@ -27,9 +27,9 @@ export const createCategory = {
         });
         await newCategory.save();
         res.status(201).json({
-            ok: true,
-            category: newCategory,
-          });
+          ok: true,
+          category: newCategory,
+        });
       }
     } catch (error) {
       res.status(400).json({
@@ -41,23 +41,42 @@ export const createCategory = {
 };
 
 export const getCategories = {
-    do: async (req, res) => {
-        const {page = 0, search} = req.params
-        const pageSize = 10
-        const categories = await Category.find(search ? {$text: {$search: searchString}} : {}).skip(page * 10).limit(pageSize)
-        res.json({
-            ok:true,
-            categories
-        })
-    }
-}
+  do: async (req, res) => {
+    const { page = 0, search } = req.params;
+    const pageSize = 10;
+    const categories = await Category.find(
+      search ? { $text: { $search: searchString } } : {}
+    )
+      .skip(page * 10)
+      .limit(pageSize);
+    res.json({
+      ok: true,
+      categories,
+    });
+  },
+};
 
 export const getAllCategories = {
-    do: async (req, res) => {
-        const categories = await Category.find()
-        res.json({
-            ok:true,
-            categories
-        })
+  do: async (req, res) => {
+    const { searchText } = req.query;
+    let categories;
+
+    if (searchText) {
+      // Usamos una expresión regular para buscar coincidencias en cualquier parte del string, ignorando mayúsculas/minúsculas
+      const regex = new RegExp(searchText, "i");
+      categories = await Category.find({ name: { $regex: regex } });
+    } else {
+      categories = await Category.find();
     }
-}
+
+    res.json({
+      ok: true,
+      categories,
+    });
+    // const categories = await Category.find()
+    // res.json({
+    //     ok:true,
+    //     categories
+    // })
+  },
+};
