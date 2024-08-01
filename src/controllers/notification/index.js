9819788a3e6d1981532652fa5864e9fe276b23a3
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-08-01 18:51:36 -0300

    [fix] GetNotifications pagination

--- src/controllers/notification/index.js
+++ src/controllers/notification/index.js
@@ -1,43 +1,44 @@
 import Notification from "../../models/notification.js";
 import mongoose from "mongoose";
 
-
-
 export const getNotifications = {
-    do: async (req, res) => {
-      const { page = 0, pageSize = 10 } = req.query;
-      const { uid } = req;
-      const notifications = await Notification.aggregate([
-        {
-            $match:{
-                notifedUser: new mongoose.Types.ObjectId(uid)
-            }
+  do: async (req, res) => {
+    const { page = 0, pageSize = 10 } = req.query;
+    const { uid } = req;
+    const notifications = await Notification.aggregate([
+      {
+        $match: {
+          notifedUser: new mongoose.Types.ObjectId(uid),
         },
-        {
-            $group: {
-              _id: "$_id",
-              count: { $sum: 1 },
-              notifedUser: { $first: "$notifedUser" },
-              notifierUser: { $first: "$notifierUser" },
-              type: { $first: "$type" },
-              targetUser: { $first: "$user" },
-              slugBlog: { $first: "$slugBlog" },
-              title: { $first: "$title" },
-              body: { $first: "$body" },
-            },
-          },
-        {
-            $facet: {
-              metadata: [{ $count: "count" }],
-              data: [{ $skip: page * pageSize }, { $limit: pageSize }],
-            },
-          },
-      ]);
-  
-      console.log("user notifications", notifications);
-      res.json({
-        ok: true,
-        notifications,
-      });
-    },
-  };
+      },
+      {
+        $group: {
+          _id: "$_id",
+          count: { $sum: 1 },
+          notifedUser: { $first: "$notifedUser" },
+          notifierUser: { $first: "$notifierUser" },
+          type: { $first: "$type" },
+          targetUser: { $first: "$user" },
+          slugBlog: { $first: "$slugBlog" },
+          title: { $first: "$title" },
+          body: { $first: "$body" },
+        },
+      },
+      {
+        $facet: {
+          metadata: [{ $count: "count" }],
+          data: [
+            { $skip: page * pageSize },
+            { $limit: parseInt(pageSize) },
+          ],
+        },
+      },
+    ]);
+
+    console.log("user notifications", notifications);
+    res.json({
+      ok: true,
+      notifications,
+    });
+  },
+};
