796abfeccef293160919d85238524c126b8e815c
Manuel Rivero <manuel.enrique.r.v@gmail.com>
2024-08-01 14:34:31 -0300

    [fix] notification message

--- src/controllers/user/index.js
+++ src/controllers/user/index.js
@@ -207,7 +207,7 @@ export const fallow = {
     const messaje = {
       notification: {
         title: `Nuevo seguidor`,
-        body: `${targetUser.name} a comenzado a seguirte`,
+        body: `${targetUser.name} ${targetUser.lastName} ha comenzado a seguirte`,
       },
       data: {
         idUserBlog: targetUser._id.toString(),
@@ -221,9 +221,9 @@ export const fallow = {
       notifedUser: targetUser._id.toString(),
       notifierUser: followUser._id.toString(),
       redirectSlug: followUser.slug,
-      type: "fallow",
+      type: "follow",
       title: `Nuevo seguidor`,
-      body: `${followUser.name} ${followUser.lastName} ha comenzado a seguirte`,
+      body: `${targetUser.name} ${targetUser.lastName} ha comenzado a seguirte`,
     });
 
     await notification.save();
